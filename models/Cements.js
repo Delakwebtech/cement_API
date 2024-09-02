6dd36908498600b70f7c9caa516307d6c1afe973
Delakwebtech <delakwebtech20@gmai.com>
2024-09-02 16:30:09 +0100

    fixed

--- models/Cements.js
+++ models/Cements.js
@@ -17,7 +17,13 @@ const CementSchema = new mongoose.Schema({
     },
     price: {
         type: Number,
-        required: [true, 'Please add a price']
+        required: [true, 'Please add price']
+    },
+    discount: {
+        type: Number,
+        default: 0,
+        min: [0, 'Discount cannot be less than 0%'],
+        max: [100, 'Discount cannot be more than 100%']
     },
     photo: {
         type: String,
@@ -29,15 +35,20 @@ const CementSchema = new mongoose.Schema({
     }
 }, 
 {
-    toJSON: {virtuals: true},
-    toObject: {virtuals: true}
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 });
-
 
 // Create cement slug from the name
 CementSchema.pre('save', function(next){
     this.slug = slugify(this.name, {lower: true});
     next();
+});
+
+// Calculate discounted price
+CementSchema.virtual('discountedPrice').get(function() {
+    const discountAmount = (this.price * this.discount) / 100;
+    return this.price - discountAmount;
 });
 
 module.exports = mongoose.model('Cements', CementSchema);
