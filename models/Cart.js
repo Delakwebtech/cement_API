58724d8aa43b91745202fdc1473f487c42aa15ac
Delakwebtech <delakwebtech20@gmai.com>
2024-09-10 21:54:16 +0100

    cart modified

--- models/Cart.js
+++ models/Cart.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 
 const CartItemSchema = new mongoose.Schema({
-  cementId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Cement',
-    required: true,
-  },
   name: {
     type: String,
     required: true,
